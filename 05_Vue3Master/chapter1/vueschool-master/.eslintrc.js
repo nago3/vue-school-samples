56642554675fb5376b63092e9db7a8f00074da4b
nago3 <hiro@nago3.xyz>
2022-04-23 17:01:20 +0900

    [Change] Separate eslintConfig settings.

---
+++ 05_Vue3Master/chapter1/vueschool-master/.eslintrc.js
@@ -0,0 +1,14 @@
+export.defaul;t =  {
+    "root": true,
+    "env": {
+        "node": true
+    },
+    "extends": [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended"
+    ],
+    "parserOptions": {
+        "parser": "@babel/eslint-parser"
+    },
+    "rules": {}
+}
